0d4ba955996c8a80309b502fc6e9cf39493a1627
Zen <nggiang12@gmail.com>
2016-12-15 04:53:46 -0600

    moveeee out the way

--- src/obj/player_selection.d
+++ src/obj/player_selection.d
@@ -1,4 +1,4 @@
-.\obj\player_selection.o: player_selection.c
+.\obj\player_selection.o: ..\game_ui\c\player_selection.c
 .\obj\player_selection.o: ..\game_ui\include\game_ui_define.h
 .\obj\player_selection.o: ..\peripherals\include\lcd.h
 .\obj\player_selection.o: C:\ProgramData\App-V\61A5EFCB-2A44-465D-90CC-A7A4A41DA0A8\D50B82DC-885B-4BF1-9479-31B1EC1F01B7\Root\ARM\ARMCC\bin\..\include\stdint.h
